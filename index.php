f089570597a8ca24d83894594efe7847e52b597f
rajahwu <vmradford@gmail.com>
2023-08-23 17:06:02 -0400

    inital commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AJAX with PHP</title>
+</head>
+<body>
+    <h1>Ajax with PHP</h1>
+</body>
+</html>
