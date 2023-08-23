7c0f03c84bcbfc96618c7f9369ce39679c65cf48
rajahwu <vmradford@gmail.com>
2023-08-23 19:38:46 -0400

    complete ajax request to php server

--- php-response/html/new_content.php
+++ php-response/html/new_content.php
@@ -1,1 +1,8 @@
-This is the new content which has been loaded by Ajax.
+<?php
+$var = 'new content';
+// ? debuging tip
+// echo $var;
+// return;
+?>
+
+<span style="color: blue;">This is the <strong> <?php echo $var ?> </strong> which has been loaded by Ajax.</span>
