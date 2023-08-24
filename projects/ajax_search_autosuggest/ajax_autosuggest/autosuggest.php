5e3861da682954565c9293bf330d664d8f32cd38
rajahwu <vmradford@gmail.com>
2023-08-24 16:12:52 -0400

    return search result

--- projects/ajax_search_autosuggest/ajax_autosuggest/autosuggest.php
+++ projects/ajax_search_autosuggest/ajax_autosuggest/autosuggest.php
@@ -11,8 +11,45 @@
     exit;
   }
 
+  // Notes:
+  // * strpos is faster than strstr or preg_match
+  // * strpos requires strict comparison operator (===)
+  //    returns 0 for match at start of string
+  //    returns false for no match
+  function str_starts_with($choice, $query) {
+    return strpos($choice, $query) === 0;
+  }
+  
+  function str_contains($choice, $query) {
+    return strpos($choice, $query) !== false;
+  }
+
+  // In real world, this would likely search a database or use
+  // a search program like Solr, Elastic Search, Sphinx, etc.
+  function search($query, $choices) {
+    $matches = [];
+    $d_query = strtolower($query);
+
+    foreach($choices as $choice) {
+      $d_choice = strtolower($choice);
+      if(str_starts_with($d_choice, $d_query)) {
+        $matches[] = $choice;
+      }
+    }
+    return $matches;
+  }
+
   $query = isset($_GET['q']) ? $_GET['q'] : '';
 
   // find and return search suggestions as JSON
+
+  $choices = file('includes/us_passenger_trains.txt', FILE_IGNORE_NEW_LINES);
+
+  $suggestions = search($query, $choices);
+  sort($suggestions);
+  $max_suggestions = 5;
+  $top_suggestions = array_slice($suggestions, 0, $max_suggestions);
+
+  echo json_encode($top_suggestions);
   
 ?>
--- projects/ajax_search_autosuggest/autosuggest.md
+++ projects/ajax_search_autosuggest/autosuggest.md
@@ -3,3 +3,13 @@
 As a user types into a text input field, the web page displays suggestions below the text input.
 
 The suggestions could be used to populate the text input field or be links to search results or a content page
+
+## Calculate Results
+
+* Autosuggest works best with large data sets
+* Over 100 records (better with 500 or 1,000 or even mor)
+* Product name, customer names, article titles
+
+This project:
+
+* United States passenger train names
