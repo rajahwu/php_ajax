8fb3cbc3f085329ba4dc69719a7bd60076eb5e37
rajahwu <vmradford@gmail.com>
2023-08-24 12:40:57 -0400

    prevent default on form submit

--- projects/ajax_form_submission/index.php
+++ projects/ajax_form_submission/index.php
@@ -29,7 +29,6 @@
         Height: <input type="text" name="height" /><br />
         <br />
         <input id="html-submit" type="submit" value="Submit" />
-        <input id="ajax-submit" type="button" value="Ajax Submit" />
       </form>
     </div>
 
@@ -45,7 +44,7 @@
 
       var result_div = document.getElementById("result");
       var volume = document.getElementById("volume");
-      var button = document.getElementById("ajax-submit");
+      var button = document.getElementById("html-submit");
       var orig_button_value = button.value;
 
 
@@ -149,7 +148,10 @@
         xhr.send(form_data);
       }
 
-      button.addEventListener("click", calculateMeasurements);
+      button.addEventListener("click", function(e) {
+        e.preventDefault();
+        calculateMeasurements();
+      });
 
     </script>
 
