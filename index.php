efe607986a3599e171cee6a1c93a780bc34825b4
rajahwu <vmradford@gmail.com>
2023-08-23 22:56:31 -0400

    add ajax form submission lesson project template

--- index.php
+++ index.php
@@ -9,20 +9,21 @@
     <a href="..">&lt; &lt; Back Home</a>
     <h1>Ajax with PHP</h1>
     <ul>
-        <li><a href="./ajax-client/">ajax-client</a></li>
-        <li><a href="./ajax-jquery/">ajax-jquery</a></li>
-        <li><a href="./ajax-json/">ajax-json</a></li>
-        <li><a href="./ajax-text/">ajax-client</a></li>
-        <li><a href="./php-dectect-request/">php-detect-request</a></li>
+        <li><a href="./ajax_client/">ajax-client</a></li>
+        <li><a href="./ajax_jquery/">ajax-jquery</a></li>
+        <li><a href="./ajax_json/">ajax-json</a></li>
+        <li><a href="./ajax_text/">ajax-client</a></li>
+        <li><a href="./php_dectect_request/">php-detect-request</a></li>
         <li>php-response</li> 
             <ul>
-                <li><a href="./php-response/html/index.html">html</a></li>
-                <li><a href="./php-response/json/index.html">json</a></li>
+                <li><a href="./php_response/html/index.html">html</a></li>
+                <li><a href="./php_response/json/index.html">json</a></li>
             </ul>
         <li><a href="./ajax_categoryselect/index.html">ajax_categoryselect</a></li>
         <li>Projects</li>
             <ul>
                 <li><a href="./projects/ajax_button/">Ajax Button</a></li>
+                <li><a href="./projects/ajax_form_submission/">Ajax Form Submission</a></li>
             </ul>
     </ul>
 </body>
---
+++ projects/ajax_form_submission/ajax-forms.md
@@ -0,0 +1,11 @@
+# AJAX Forms
+
+## User cases
+
+* User log in
+* Contact us, feedback
+* Polls, surveys
+* Facebook post, Twitter tweet
+* Dictionary, phrase translator
+* File upload
+* In-place content editing
